256232f316dd6d79da63d1051e8a494ae37c5d19
danilocamposdev <danilocampos.dev@gmail.com>
2026-06-26 17:24:12 -0300

    fix: add produced_quantity

--- src/model/Order.h
+++ src/model/Order.h
@@ -11,10 +11,12 @@ class Order {
 		int client_id_;
 		int quantity_;
 		Priority priority_;
+		int produced_quantity_;
 	public:
-		Order(Date delivery_date, int mold_id, int client_id, int quantity, Priority priority)
+		Order(Date delivery_date, int mold_id, int client_id, int quantity, Priority priority, int produced_quantity=0)
 			: delivery_date_{delivery_date}, mold_id_{mold_id},
-			client_id_{client_id}, quantity_{quantity}, priority_{priority} {}
+			client_id_{client_id}, quantity_{quantity}, priority_{priority},
+			produced_quantity_{produced_quantity}{}
 
 		std::optional<int> id() const { return id_; }
 		void setId(int id) { id_ = id; }
@@ -24,4 +26,5 @@ class Order {
 		int client_id() const { return client_id_; }
 		int quantity() const { return quantity_; }
 		Priority priority() const { return priority_; }
+		int produced_quantity() const { return produced_quantity_; }
 };
