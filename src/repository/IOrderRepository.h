84ba1e2591fcaf21172d390ac43efa4a2733547e
danilocamposdev <danilocampos.dev@gmail.com>
2026-06-26 15:18:32 -0300

    fix: save returns persisted Order

--- src/repository/IOrderRepository.h
+++ src/repository/IOrderRepository.h
@@ -5,7 +5,7 @@
 
 class IOrderRepository {
 public:
-    virtual void add(Order order) = 0;
+    virtual Order save(Order order) = 0;
     virtual std::optional<Order> get_by_id(int id) const = 0;
     virtual std::vector<Order> all() const = 0;
     virtual ~IOrderRepository() = default;
